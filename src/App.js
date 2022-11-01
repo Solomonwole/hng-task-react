f6b6d4cd980cfbe1c0849c5672a910d006c4f062
Solomon <solomonwolemail@gmail.com>
2022-11-01 13:39:20 +0100

    Added padding and made the page responsive

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Navbar from './components/Navbar/Navbar'
 import Avatar from './components/Avatar/Avatar'
 import Link from './components/Links/Link'
 import Social from './components/Social/Social'
+import './Container.css'
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header container">
         <Navbar />
         <Avatar/>
         <Link/>
