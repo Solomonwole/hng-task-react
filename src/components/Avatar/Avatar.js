45cb449489b095a47196d55d3f3d950906aa7bc8
Solomon <solomonwolemail@gmail.com>
2022-11-01 13:53:28 +0100

    Made username open in a new tab

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import profile_img from '../profile_img.png'
-import './Avatar.css'
-
+import React from "react";
+import profile_img from "../profile_img.png";
+import "./Avatar.css";
 
 function avatar() {
   return (
     <>
-      <div className='profile-image'>
-        <img src={profile_img} alt="Profile Image" id='profile__img'/>
+      <div className="profile-image">
+        <img src={profile_img} alt="Profile Image" id="profile__img" />
       </div>
-      <p className='username' id='twitter'><a href="https://twitter.com/tposhofficial">tposhofficial</a></p>
-      <p className='username' id='slack' hidden> SolomonWole</p>
+      <p className="username" id="twitter">
+        <a href="https://twitter.com/tposhofficial" target='_blank'>tposhofficial</a>
+      </p>
+      <p className="username" id="slack" hidden>
+        {" "}
+        SolomonWole
+      </p>
     </>
-  )
+  );
 }
 
-export default avatar
+export default avatar;
